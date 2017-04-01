756fd13461212d690d2677906256562edfcc15f4
chesteric31 <chesteric31@gmail.com>
2017-04-01 15:09:02 +0200

    Import project from chapter 3 (Angular2 Development with Typescript)

--- app/components/product-item/product-item.ts
+++ app/components/product-item/product-item.ts
@@ -1,11 +1,11 @@
 import {Component, Input} from '@angular/core';
-import StarsComponent from 'app/components/stars/stars';
-import {Product} from 'app/services/product-service';
+import {Product} from '../../services/product-service';
 
 @Component({
-	selector: 'auction-product-item',
-	templateUrl: 'app/components/product-item/product-item.html'
+  selector: 'auction-product-item',
+  styleUrls: ['app/components/product-item/product-item.css'],
+  templateUrl: 'app/components/product-item/product-item.html',
 })
 export default class ProductItemComponent {
-	@Input() product: Product;
+  @Input() product: Product;
 }
